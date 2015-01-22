8cbbdbedf688983793c17259e77180f7d22bb313
AnaCorral <Ana.Corral@kinneirdufort.com>
2015-01-22 14:18:21 +0000

    Powers

--- js/score.js
+++ js/score.js
@@ -1,1 +1,14 @@
 // JavaScript Document
+
+$( document ).ready(function() {
+    // grap the text on H3 tag
+    var power = $("h3").contents().filter(function() {
+        return this.nodeType == 3;
+    });
+    
+    // grap the number on the text from H3
+    power = power[0].textContent.replace(/[^0-9]/gi, '');
+    
+    // show the number and transform it in base 10
+    alert(parseInt(power, 10));
+});
