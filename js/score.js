849d311dee53a1d7c32e983a20f0e9389c44041d
Ana <anacorral29@gmail.com>
2015-01-22 07:39:12 +0000

    Changing class and message depending of score
    I needed to change a little the html, I will send you the changes by
    email.

--- js/score.js
+++ js/score.js
@@ -1,1 +1,9 @@
 // JavaScript Document
+
+function myFunction() {
+    var age,voteable;
+    age = document.getElementById("age").value;
+    voteable = (age < 18) ? "Too young":"Old enough";
+    document.getElementById("demo").innerHTML = voteable + " to vote.";
+}
+
