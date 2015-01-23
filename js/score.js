d7e97c3983d539aad4ff200febb0f0ad716bfbc8
Ana <anacorral29@gmail.com>
2015-01-23 07:46:22 +0000

    last changes

--- js/score.js
+++ js/score.js
@@ -1,19 +1,14 @@
 // JavaScript Document
 
-
-$( document ).ready(function() {
-    // grap the text on input tag
-    var power = $("input").contents().filter(function() {
-        return this.nodeType == 3;
-    });
+//$( document ).ready(function() {
+    // grap the text on H3 tag
+   // var power = $("h3").contents().filter(function() {
+       // return this.nodeType == 3;
+   // });
     
-    // grap the number on the text from input
-    power = power[0].textContent.replace(/[^0-9]/gi, '');
+    // grap the number on the text from H3
+   // power = power[0].textContent.replace(/[^0-9]/gi, '');
     
     // show the number and transform it in base 10
-    alert(parseInt(power, 10));
-});
-
-
-
-
+    //alert(parseInt(power, 10));
+//});
